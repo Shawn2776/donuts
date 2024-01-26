dc378018fe8e1b28336575f5215132e36e98211a
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-01-25 17:21:14 -0800

    tab work

--- components/Tabs.jsx
+++ components/Tabs.jsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { useState } from "react";
 import Donuts from "../data";
 import Accordion from "@/components/Accordion";
 
 const Tabs = () => {
+  const [isActive, setIsActive] = useState("donuts");
+
   const classics = Donuts.filter((donut) => donut.type === "classic");
   const deluxes = Donuts.filter((donut) => donut.type === "deluxe");
   const others = Donuts.filter((donut) => donut.type === "other");
@@ -18,21 +23,34 @@ const Tabs = () => {
 
   return (
     <div className="flex flex-col">
-      <div className="flex justify-between w-full mb-4 text-2xl">
-        <div className="flex justify-center w-full py-4 border rounded-md">
+      <div className="flex justify-between w-full mb-1 text-2xl">
+        <div
+          className={`flex justify-center w-full py-4 border rounded-md ${
+            isActive === "donuts" ? "bg-secondary" : ""
+          }`}
+          onClick={() => setIsActive("donuts")}
+        >
           Donuts
         </div>
-        <div className="flex justify-center w-full py-4 border rounded-md">
+        <div
+          className={`flex justify-center w-full py-4 border rounded-md ${
+            isActive === "donuts" ? "" : "bg-secondary"
+          }`}
+          onClick={() => setIsActive("drinks")}
+        >
           Drinks
         </div>
       </div>
       <div>
-        <div className="w-full">
+        <div className={`${isActive === "donuts" ? "" : "hidden"} w-full`}>
           <Accordion accordionInfo={donutInfo} />
         </div>
-        {/* <div className="w-full">
-          <Accordion accordionInfo={drinkInfo} />
-        </div> */}
+        <div className={`${isActive === "donuts" ? "hidden" : ""} w-full`}>
+          <h2 className="flex justify-center h-screen mt-20 text-2xl align-middle">
+            Coming Soon
+          </h2>
+          {/* <Accordion accordionInfo={drinkInfo} /> */}
+        </div>
       </div>
     </div>
   );
